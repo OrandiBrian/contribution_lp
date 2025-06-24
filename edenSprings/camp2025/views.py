67ee548f4a1f4d385281d1eb712727c4fdd67af4
Brian Orandi <brianorandi97@gmail.com>
2025-06-24 10:57:54 +0300

    Added environment variables to views.py

--- edenSprings/camp2025/views.py
+++ edenSprings/camp2025/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .forms import PaymentForm
+from dotenv import load_dotenv
+import os
+
+# loading environment variables
+load_dotenv()
+
+# retrieving variables from environment
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+MPESA_PASSKEY = os.getenv('MPESA_PASSKEY')
+MPESA_BASE_URL = os.getenv('MPESA_BASE_URL')
+MPESA_SHORTCODE = os.getenv('MPESA_SHORTCODE')
+CALLBACK_URL = os.getenv('CALLBACK_URL')
 
 # Create your views here.
 def payment_view(request):
