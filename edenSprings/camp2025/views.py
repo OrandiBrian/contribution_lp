af055f7df0f500fcbc2345c3235a49ac03b18765
Brian Orandi <brianorandi97@gmail.com>
2025-06-24 10:22:23 +0300

    Made migrations and added payment view to views.py

--- edenSprings/camp2025/views.py
+++ edenSprings/camp2025/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import PaymentForm
 
 # Create your views here.
-def index(request):
-    return render(request, 'camp2025/index.html')
+def payment_view(request):
+    if request.method == 'POST':
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            full_name = form.cleaned_data['full_name']
+            phone_number = form.cleaned_data['phone_number']
+            amount = form.cleaned_data['amount']
+
+            
+
+    else:
+        # display the form
+        form = PaymentForm()
+        context = {'form': form}
+    return render(request, 'camp2025/index.html', context)
