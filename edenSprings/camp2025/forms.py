8ac2cba6aeb4501e1aa9de73bf962910ae169ac0
Brian Orandi <brianorandi97@gmail.com>
2025-06-24 10:16:38 +0300

    Added forms.py

--- edenSprings/camp2025/forms.py
+++ edenSprings/camp2025/forms.py
@@ -1,5 +1,18 @@
 from django import forms 
 
 class PaymentForm(forms.Form):
-    phone_number = forms.CharField(max_length=15, required=True, label='Phone Number')
-    amount = forms.IntegerField(min_value=1, required=True, label='Amount')
+    full_name = forms.CharField(label='Full Name',
+                                required=True,
+                                max_length=50,
+                                widget=forms.TextInput(attrs={'placeholder': 'Enter full name', 'class': 'input input-bordered w-full'}))
+    phone_number = forms.CharField(max_length=15, 
+                                   required=True, 
+                                   label='Phone Number',
+                                   widget=forms.TextInput(attrs={'placeholder': '2547XXXXXXXX',
+                                                                #  'pattern': '^(\+254|0)[17]\\d{8}$',
+                                                                 'class': 'input input-bordered w-full'}))
+    amount = forms.IntegerField(min_value=1, 
+                                required=True, 
+                                label='Amount',
+                                widget=forms.NumberInput(attrs={'placeholder': 'Enter amount',
+                                                                'class': 'input input-bordered w-full'}))
